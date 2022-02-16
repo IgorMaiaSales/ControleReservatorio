c1d19a886eece40411e647ab85722fe4b9c0d77e
Igor Maia <igormaiasales@gmail.com>
2022-02-16 19:57:42 -0300

    Corrigindo erros

--- Release/Core/Src/keypad.d
+++ Release/Core/Src/keypad.d
@@ -22,7 +22,8 @@ Core/Src/keypad.o: ../Core/Src/keypad.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Core/Inc/keypad.h
 
 ../Core/Inc/main.h:
 
@@ -73,3 +74,5 @@ Core/Src/keypad.o: ../Core/Src/keypad.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+../Core/Inc/keypad.h:
